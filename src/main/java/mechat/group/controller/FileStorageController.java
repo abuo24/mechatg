e9801b85afe9aa222b3c4afa936b6bd86cc890a5
mryed <mryediniofficial9924@gmail.com>
2020-12-23 20:55:56 +0500

    files upload packege is remove

--- src/main/java/mechat/group/controller/FileStorageController.java
+++ src/main/java/mechat/group/controller/FileStorageController.java
@@ -1,7 +1,6 @@
 package mechat.group.controller;
 
 import mechat.group.entity.FileStorage;
-import mechat.group.model.ResultSucces;
 import mechat.group.repository.FileStorageRepo;
 import mechat.group.service.FileStorageService;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,12 +9,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.net.URLEncoder;
-
-import static com.sun.deploy.util.BufferUtil.MB;
 
 @RestController
 @RequestMapping("/api/file")
@@ -23,20 +19,19 @@ public class FileStorageController {
 
     private final FileStorageService fileStorageService;
 
-    private  final FileStorageRepo fileStorageRepository;
     @Value("${upload.folder}")
+
     private String uploadFolder;
 
-    public FileStorageController(FileStorageService fileStorageService, FileStorageRepo fileStorageRepository) {
+    public FileStorageController(FileStorageService fileStorageService) {
         this.fileStorageService = fileStorageService;
-        this.fileStorageRepository = fileStorageRepository;
     }
 
     @GetMapping("/preview/{hashId}")
     public ResponseEntity preview(@PathVariable String hashId) throws IOException {
-        FileStorage fileStorage =  fileStorageService.findByHashId(hashId);
+        FileStorage fileStorage = fileStorageService.findByHashId(hashId);
         return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; fileName=\""+ URLEncoder.encode(fileStorage.getName()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; fileName=\"" + URLEncoder.encode(fileStorage.getName()))
                 .contentType(MediaType.parseMediaType(fileStorage.getContentType()))
                 .body(new FileUrlResource(String.format("%s/%s",
                         uploadFolder,
@@ -45,9 +40,9 @@ public class FileStorageController {
 
     @GetMapping("/download/{hashId}")
     public ResponseEntity download(@PathVariable String hashId) throws IOException {
-        FileStorage fileStorage =  fileStorageService.findByHashId(hashId);
+        FileStorage fileStorage = fileStorageService.findByHashId(hashId);
         return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; fileName=\""+ URLEncoder.encode(fileStorage.getName()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; fileName=\"" + URLEncoder.encode(fileStorage.getName()))
                 .contentType(MediaType.parseMediaType(fileStorage.getContentType()))
                 .body(new FileUrlResource(String.format("%s/%s",
                         uploadFolder,
